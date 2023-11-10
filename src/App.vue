8092db59129426de9cbc0a85587fef4f195113eb
Animakkon <lada.dunskaya@mail.ru>
2023-11-10 20:47:06 +0300

    + links - unused

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,8 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <!-- <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div> -->
-
-  <p>my first try to page</p>
-    <router-link to="/helloWorldPage">Hello</router-link>
+    <router-link class="btn btn-link" to="/">HomePage</router-link>
+    <router-link class="btn btn-link" to="/helloWorldPage">Standard vue start progect</router-link>
     <router-view></router-view>
-
-
-  <!-- TODO: create home page -->
-  <!-- TODO: name title page and icon in browser -->
-
-  
-  <!-- <HelloWorld msg="Vite + Vue" /> -->
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
