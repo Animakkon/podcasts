ce5139b84a08debc5eb51644dcf5571bfda788eb
Animakkon <lada.dunskaya@mail.ru>
2024-01-27 22:28:21 +0300

    page template

---
+++ src/components/PageTemplate.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import Actions from '@/components/HeaderActions.vue'
+</script>
+
+<template>
+  <header>
+    <Actions/>
+
+    <div>
+      <slot name="header">123</slot>
+    </div>
+  </header>
+
+  <main>
+    <slot name="main"></slot>
+  </main>
+
+  <footer>
+    <slot name="footer"></slot>
+  </footer>
+
+</template>
+
+<style scoped>
+header, main, footer {
+  padding: 0.5rem;
+  border: 1px dotted black;
+}
+
+</style>
