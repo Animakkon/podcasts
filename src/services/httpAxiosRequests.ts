fd49eb4ac0e83839d7473987145f92120550a0ee
Animakkon <lada.dunskaya@mail.ru>
2024-01-31 08:18:00 +0300

    axios put post methods

--- src/services/httpAxiosRequests.ts
+++ src/services/httpAxiosRequests.ts
@@ -2,10 +2,25 @@ import axios from "axios";
 
 const BASE_URL = "https://httpbin.org"
 
-export async function get(url: string, headers?: {[k: string]: any}, place?: string) {
-    return axios.get( `${BASE_URL}/${url}`, {
+export async function get(url: string, headers?: {[k: string]: any}, base?: string, place?: string) {
+    const str = base ? base : BASE_URL
+
+    return axios.get( `${str}/${url}`, {
         headers: headers
     })
         .then(response => response.data )
         .catch(() => console.log('$Get ERROR$: ', place));
 }
+
+export async function post(url: string, body:string, headers?: {[k: string]: any}, base?: string, place?: string) {
+    const str = base ? base : BASE_URL
+
+    return axios.get( `${str}/${url}`, {
+        headers: headers
+    })
+        .then(response => {
+          return   response.status === 200
+        } )
+        .catch(() => console.log('$Get ERROR$: ', place));
+}
+
