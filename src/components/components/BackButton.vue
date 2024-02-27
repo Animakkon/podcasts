8a7c5fd627f1d98d66e7246ea7f5a454b9d7d589
Animakkon <lada.dunskaya@mail.ru>
2024-02-27 21:51:26 +0300

    кнопка обратно

---
+++ src/components/components/BackButton.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-btn @click="$router.go(-1)"
+         density="comfortable"
+         color="orange-darken-1"
+         variant="text"
+  >
+    <span style="color: grey">Обратно</span></v-btn>
+</template>
+
+<style>
+
+</style>
