e6c73777f7455aaa72590c52240150bc0978ca2a
Animakkon <lada.dunskaya@mail.ru>
2024-02-29 21:26:58 +0300

    fix: верстка отступы кнопок

--- src/components/components/AddThenGoToCartButton.vue
+++ src/components/components/AddThenGoToCartButton.vue
@@ -11,19 +11,17 @@ function emitProductToCart() {
 
 <template>
   <v-btn v-show="!isInCart"
-         @click.stop.prevent="$emit('emitProductToCart',productId)"
-         size="x-large"
+         @click.stop.prevent="emitProductToCart()"
          density="comfortable"
-         class="text-none text-black ma-0 pa-0"
+         class="text-none text-black pa-0 mb-4"
          color="green-darken-1"
          block>
     Добавить
   </v-btn>
   <v-btn @click.stop.prevent="$router.push({ name: 'Cart'})"
          v-show="isInCart"
-         size="x-large"
          density="comfortable"
-         class="text-none text-green-lighten-5 ma-0 pa-0"
+         class="text-none text-green-lighten-5 pa-0 mb-4"
          color="green-lighten-4"
          block>
     В корзине
