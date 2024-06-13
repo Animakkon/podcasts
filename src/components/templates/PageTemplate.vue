57bde18bd27f40f74b543c650a1abbdc5df97f4c
Animakkon <lada.dunskaya@mail.ru>
2024-06-13 10:34:03 +0300

    лого в темплейт страниц

--- src/components/templates/PageTemplate.vue
+++ src/components/templates/PageTemplate.vue
@@ -1,22 +1,36 @@
 <script setup lang="ts">
 import Actions from '@/components/general/HeaderActions.vue'
 import NavBtn from '@/components/general/NavigationButton.vue'
+
+import logoImage from '@assets/images/logo.png'
 </script>
 
 <template>
-  <header class="d-flex header sticky">
-    <NavBtn/>
+  <div>
+    <header class="d-flex header sticky">
+      <div class="d-flex header__left-side">
+        <div class="logo" @click="$router.push('/shop')">
+          <v-img :src="logoImage" :width="'100px'" :height="'100%'" class="mr-5"></v-img>
+        </div>
 
-    <div id="page_header_content">
-      <slot name="header"></slot>
+        <NavBtn/>
+      </div>
+
+      <div class="d-flex header__right-side">
+
+        <div id="page_header_content">
+          <slot name="header"></slot>
+        </div>
+
+        <Actions/>
+      </div>
+
+    </header>
+
+    <div class="main_article">
+      <slot name="main"></slot>
     </div>
-
-    <Actions/>
-  </header>
-
-  <article class="main_article">
-    <slot name="main"></slot>
-  </article>
+  </div>
 
 </template>
 
@@ -27,12 +41,24 @@ header, article {
   padding: 0px 72px;
 }
 
+.logo, #page_header_content {
+  mar-right: 20px;
+}
+
+.logo:hover {
+  cursor: pointer;
+}
+
 .header {
   background-color: var(--on-surface-variant);
   justify-content: space-between;
+}
+
+.header__right-side {
+  flex: 0 2 560px;
 
   #page_header_content {
-    flex: 1 2 512px
+    flex: 0 2 512px
   }
 }
 
