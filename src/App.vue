dcc6f3818fe24e0c7cd942923b1f1d1eb7e1a200
Animakkon <lada.dunskaya@mail.ru>
2024-06-13 10:33:28 +0300

    загрузка каталога при открытии приложения

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,11 @@
 <script setup lang="ts">
+import {onMounted} from "vue";
+import {useProductsStore} from "@/store-pinia/productsStore.ts";
+
+const _productsStore = () => useProductsStore()
+onMounted(() => {
+  _productsStore().GET_LIST()
+})
 </script>
 
 <template>
