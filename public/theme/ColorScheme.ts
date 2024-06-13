182103bfed6fc5bd4cd10b63af5422aa53964786
Animakkon <lada.dunskaya@mail.ru>
2024-06-13 10:30:31 +0300

    попытка в свои цвета

---
+++ public/theme/ColorScheme.ts
@@ -0,0 +1,23 @@
+import {ThemeDefinition} from "vuetify";
+
+const pinkCustomTheme: ThemeDefinition = {
+    dark: false,
+    colors: {
+        background: '#FFFFFF',
+        surface: '#FFFFFF',
+        'surface-bright': '#FFFFFF',
+        'surface-light': '#E4EEF2',
+        'surface-variant': '#4D4D4D',
+        primary_light: '#F8EFF2',
+        primary: '#C57F99',
+        primary_darken_1: '#96455e',
+        secondary: '#E4EEF2',
+        secondary_darken_1: '#7e98b1',
+        error: '#B00020',
+        info: '#03e1d6',
+        success: '#00c41c',
+        warning: '#FBD05D',
+    }
+}
+
+export default pinkCustomTheme
