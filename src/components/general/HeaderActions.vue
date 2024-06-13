3c656c484b19d2d23b10ae937cb09ec2e640be73
Animakkon <lada.dunskaya@mail.ru>
2024-06-13 10:40:21 +0300

    кнопки на кастомный компонент + открытие модалки на инфо пользователя

--- src/components/general/HeaderActions.vue
+++ src/components/general/HeaderActions.vue
@@ -1,81 +1,61 @@
-<script setup lang="ts">
-import menuItems from '@/router/menuItems.js'
-import {computed, onBeforeMount, onMounted, reactive, ref} from "vue";
-import {isAuthorized, logout} from "@/services/auth.ts";
-import {getCartProducts, getCartTotalsSum} from "@/services/cart.ts";
-const menu = menuItems;
+<script lang="ts" setup>
+import IBtn from "@/components/components/ui/IconButton.vue";
+
+import {computed, onMounted, ref} from "vue";
 import {isAuthorized, logout} from "@/services/data/auth.js";
 import {getCartProducts, getCartTotalsSum} from "@/services/data/cart.js";
 
-let authorized = ref(false)
+import EWDialog from "@/components/components/EWDialog.vue";
+
+const isShowLogout = computed(() => isAuthorized())
+
 let cartCounts = ref(0)
 
 onMounted(() => {
-  authorized = isAuthorized()
   cartCounts = getCartTotalsSum()
+})
+
+const checkCart = computed(() => getCartProducts().length > 0)
+const buildCartVariant = computed(() => {
+  return checkCart.value ? 'tonal' : 'text';
 })
 
 function toLogOut() {
   logout()
-  window.location.reload()
 }
-
-const checkCart = computed(() => getCartProducts().length > 0)
 
 </script>
 
 <template>
-  <v-container>
-    <div id="navigation">
-      <v-btn variant="tonal">
-        <v-icon icon="mdi-menu" color="grey-darken-3"></v-icon>
-        <v-menu activator="parent">
-          <v-list>
-            <v-list-item
-                v-for="(item, index) in menu"
-                :key="index"
-                :value="index">
-              <v-list-item-title
-                  @click="$router.push(item.link)">
-                {{ item.title }}
-              </v-list-item-title>
-            </v-list-item>
-          </v-list>
-        </v-menu>
-      </v-btn>
+  <div class="d-flex ml-10">
+    <IBtn :color="'primary_darken_1'"
+          :icon="'mdi-cart-variant'"
+          :text="'Корзина'"
+          :variant="buildCartVariant"
+          @click="$router.push('/cart')"
+    ></IBtn>
+
+    <div v-show="isShowLogout">
+      <EWDialog></EWDialog>
     </div>
 
-    <div id="user-actions">
-      <v-btn @click="$router.push('/cart')"
-             :color="checkCart ? 'green-darken-2' : 'grey-lighten-5' "
-             variant="tonal">
-        <v-icon icon="mdi-cart-variant"
-                color="grey-darken-3"
-        ></v-icon>
-      </v-btn>
-      <v-btn  variant="text" :disabled="true">
-        <v-icon icon="mdi-account-check" color="grey-darken-3"></v-icon>
-      </v-btn>
-      <v-btn  v-show="authorized" @click="toLogOut" variant="text">
-        <v-icon icon="mdi-logout" color="grey-darken-3"></v-icon>
-      </v-btn>
-    </div>
-  </v-container>
+    <IBtn v-show="isShowLogout"
+          :color="'primary_darken_1'"
+          :icon="'mdi-logout'"
+          :text="'Выход'"
+          @click="toLogOut"
+    ></IBtn>
+
+    <IBtn v-show="!isShowLogout"
+          :color="'primary_darken_1'"
+          :icon="'mdi-login'"
+          :text="'Вход'"
+          @click="$router.push('/login')"
+    ></IBtn>
+
+  </div>
+
 </template>
 
 <style scoped>
-#navigation {
-  position: absolute;
-  left: 0.5rem;
-}
-
-#user-actions {
-  position: absolute;
-  right: 0.5rem;
-
-  .v-btn {
-    margin-right: 0.1rem;
-  }
-}
-
 </style>
